4d9ce93cb27951f6c63e5bea59ce4c53bff88869
Ben Driggs <bendriggs99@gmail.com>
2026-02-01 17:38:07 -0700

    Removed treatments table. Added treatment and response to subjects table.

--- database/initialize_db.py
+++ database/initialize_db.py
@@ -30,28 +30,19 @@ def create_database(cell_count):
                 condition TEXT NOT NULL,
                 age INTEGER NOT NULL,
                 sex TEXT NOT NULL,
-                FOREIGN KEY (project_id) REFERENCES projects(project_id)
-            );""")
-            
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS treatments (
-                treatment_id INTEGER PRIMARY KEY,
-                subject_id INTEGER NOT NULL,
                 treatment TEXT NOT NULL,
                 response TEXT,
-                FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+                FOREIGN KEY (project_id) REFERENCES projects(project_id)
             );""")
             
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS samples (
                 sample_id INTEGER PRIMARY KEY,
                 subject_id INTEGER NOT NULL,
-                treatment_id INTEGER NOT NULL,
                 time_from_treatment_start INTEGER NOT NULL,
                 sample TEXT NOT NULL,
                 sample_type TEXT NOT NULL,
-                FOREIGN KEY (subject_id) REFERENCES subjects(subject_id),
-                FOREIGN KEY (treatment_id) REFERENCES treatments(treatment_id)
+                FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
             );""")
             
         cursor.execute("""
@@ -74,22 +65,16 @@ def create_database(cell_count):
             SELECT DISTINCT project
             FROM raw_data;
             
-            INSERT INTO subjects (project_id, subject, condition, age, sex)
-            SELECT projects.project_id, subject, condition, age, sex
+            INSERT INTO subjects (project_id, subject, condition, age, sex, treatment, response)
+            SELECT projects.project_id, subject, condition, age, sex, treatment, response
             FROM raw_data r
             JOIN projects ON projects.project_name = r.project
             GROUP BY projects.project_id, r.subject;
             
-            INSERT INTO treatments (subject_id, treatment, response)
-            SELECT subjects.subject_id, treatment, response
+            INSERT INTO samples (subject_id, sample, sample_type, time_from_treatment_start)
+            SELECT subjects.subject_id, sample, sample_type, time_from_treatment_start
             FROM raw_data r
             JOIN subjects ON subjects.subject = r.subject;
-            
-            INSERT INTO samples (subject_id, treatment_id, sample, sample_type, time_from_treatment_start)
-            SELECT subjects.subject_id, treatments.treatment_id, sample, sample_type, time_from_treatment_start
-            FROM raw_data r
-            JOIN subjects ON subjects.subject = r.subject
-            JOIN treatments on treatments.subject_id = subjects.subject_id AND treatments.treatment = r.treatment;
             
             INSERT INTO results (sample_id, b_cell, cd8_t_cell, cd4_t_cell, nk_cell, monocyte)
             SELECT samples.sample_id, b_cell, cd8_t_cell, cd4_t_cell, nk_cell, monocyte
